0f309c369a92ecaba0afe4215271eb85897b23d6
Mackenzie Hatfield <therealbakoonzar@gmail.com>
2020-02-12 18:38:45 -0800

    Comment out dynamo db stuff for now.

--- index.js
+++ index.js
@@ -5,27 +5,49 @@ const AWS = require('aws-sdk');
 
 AWS.config.update({region: 'us-east-1'});
 
-const dynamodb = new AWS.DynamoDB();
+// const dynamodb = new AWS.DynamoDB();
 
 app.get('/', (req, res) => res.send('Fake Spotify Like App'));
 
-app.get('/artists', (req, res) => {
-  const params = {
-    ExpressionAttributeNames: {
-      "#AN": "ArtistName"
-    },
-    ProjectionExpression: "#AN",
-    TableName: "MusicTable"
-  };
+// app.get('/artists', (req, res) => {
+//   const params = {
+//     ExpressionAttributeNames: {
+//       "#AN": "ArtistName"
+//     },
+//     ProjectionExpression: "#AN",
+//     TableName: "MusicTable"
+//   };
 
-  dynamodb.scan(params, (err, data) => {
-    if (err) {
-      console.log(err, err.stack);
-    } else {
-      res.send(data);
-    }
-  })
+//   dynamodb.scan(params, (err, data) => {
+//     if (err) {
+//       console.log(err, err.stack);
+//     } else {
+//       res.send(data);
+//     }
+//   });
+// });
 
-})
+// app.get('/genres', (req, res) => {
+//   const params = {
+//     ExpressionAttributeNames: {
+//       "#G": "Genre"
+//     },
+//     ProjectionExpression: "#G",
+//     TableName: "MusicTable"
+//   };
+
+//   dynamodb.scan(params, (err, data) => {
+//     if (err) {
+//       console.log(err, err.stack);
+//     } else {
+//       res.send(data);
+//     }
+//   });
+// });
+
+// app.get('/songs', (req, res) => {
+//   const { song } = req.query
+//   res.send(song);
+// })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
