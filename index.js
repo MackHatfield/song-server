b737651525ffc171a6ad9bc47ff5c1ca7a670933
Mackenzie Hatfield <therealbakoonzar@gmail.com>
2020-02-13 18:23:20 -0800

    Install lodash.

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const AWS = require('aws-sdk');
+const _ = require('lodash');
 
 AWS.config.update({region: 'us-east-1'});
 
 // const dynamodb = new AWS.DynamoDB();
+const s3 = new AWS.S3();
+
+const bucketName = 'songuploaderbucket2983902'
 
 app.get('/', (req, res) => res.send('Fake Spotify Like App'));
 
@@ -50,4 +54,23 @@ app.get('/', (req, res) => res.send('Fake Spotify Like App'));
 //   res.send(song);
 // })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/artists', (req, res) => {
+  const params = {
+    Bucket: bucketName,
+  };
+  s3.listObjects(params, (err, data) => {
+    if (err) console.log(err, err.stack); // an error occurred
+    else {
+      const itemList = data.Contents;
+      let artists = itemList.map(item => {
+        let artistName = item.Key.split('/')[1]
+        return artistName;
+      });
+      
+      const uniqueArtists = _.uniq(artists);
+      res.send(uniqueArtists)
+    }
+  });
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
